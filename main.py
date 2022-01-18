382b538b1f4a49ee9e482abc58c4c70b460707bb
dwissaaj <dwis.buntelman123@gmail.com>
2022-01-18 10:23:51 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+class TVM():
+
+    def get_ear(self,ear,account,periodic_rate,month,annual_rate) -> float:
+        self.ear = ear
+        self.account = account
+        self.periodic_rate = periodic_rate
+        self.month = month
+        self.annual_rate = annual_rate
+
+        ear = (1+ )
