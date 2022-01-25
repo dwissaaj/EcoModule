83151099ed9439f787d530eb5c911433f16cb384
dwissaaj <dwis.buntelman123@gmail.com>
2022-01-25 10:22:34 +0700

    ear function update

--- main.py
+++ main.py
@@ -7,8 +7,17 @@ class TVM():
         i = i - 1
         return i
 
-    print(get_ear(8,4))
+    print(get_ear(8, 4))
 
+    def predict_by_ear(account,r,m,year):
+        r = r / 100
+        i = 1 + r / m
+        i = pow(i, m)
+        i = i - 1
+        account = account * i * year
+        print(pow(i,m))
+        return account
 
+    print(predict_by_ear(2500,8,4,2))
 
 
